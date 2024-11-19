8c69b0697dd743fdbeb21756ca3963178dfb8d72
MAXEL599 <carlosururchurt@gmail.com>
2024-11-18 22:47:50 -0700

    Agregar archivos de proyecto.

---
+++ proyecto interpolación de Newton/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace proyecto_interpolación_de_Newton
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            label3 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(284, 108);
+            label1.Name = "label1";
+            label1.Size = new Size(24, 24);
+            label1.TabIndex = 1;
+            label1.Text = "Y";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(182, 108);
+            label2.Name = "label2";
+            label2.Size = new Size(24, 24);
+            label2.TabIndex = 2;
+            label2.Text = "X";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(153, 236);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(79, 38);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(261, 162);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(79, 38);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(153, 162);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(79, 38);
+            textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(261, 408);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(79, 38);
+            textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(261, 236);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(79, 38);
+            textBox5.TabIndex = 8;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(261, 315);
+            textBox6.Multiline = true;
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(79, 38);
+            textBox6.TabIndex = 9;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(153, 408);
+            textBox7.Multiline = true;
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(79, 38);
+            textBox7.TabIndex = 10;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(153, 315);
+            textBox8.Multiline = true;
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(79, 38);
+            textBox8.TabIndex = 11;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(538, 379);
+            textBox9.Multiline = true;
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(512, 176);
+            textBox9.TabIndex = 12;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(527, 339);
+            label3.Name = "label3";
+            label3.Size = new Size(131, 24);
+            label3.TabIndex = 13;
+            label3.Text = "RESULTADO";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(649, 140);
+            button1.Name = "button1";
+            button1.Size = new Size(240, 60);
+            button1.TabIndex = 14;
+            button1.Text = "calcular";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(649, 236);
+            button2.Name = "button2";
+            button2.Size = new Size(240, 60);
+            button2.TabIndex = 15;
+            button2.Text = "LIMIPIAR  DATOS";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 24F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1384, 1060);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(textBox9);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Arial", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(6, 4, 6, 4);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private Label label3;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ proyecto interpolación de Newton/Form1.cs
@@ -0,0 +1,10 @@
+namespace proyecto_interpolación_de_Newton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
